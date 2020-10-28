e436dbcd172a8acb231164432ebaeede2307e630
nonx0110 <19s005oftn@gmail.com>
2020-10-28 12:16:31 +0900

    2020/10/28/12:16

--- database/seeds/TodoTableSeeder.php
+++ database/seeds/TodoTableSeeder.php
@@ -10,7 +10,16 @@ class TodoTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {   
+        for($i = 1; $i <= 10; $i++){
+            DB::table('user')->insert([
+                'name' => "ユーザー$i",
+                'email' => "hoge+$i@test.com",
+                'password' => bcrypt('password'),
+            ]);
+        }
+
+
         for($i = 1; $i <= 100; $i++){
             DB::table('todos')->insert([
                 'user_id' => $this->createUserIdByTaskId($i),
